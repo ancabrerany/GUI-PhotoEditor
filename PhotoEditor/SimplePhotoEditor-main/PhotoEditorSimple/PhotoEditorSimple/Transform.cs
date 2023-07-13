17654f04a06d3f92be176b3722122ba7c89dc48c
Anny Cabrera <acabrera@harding.edu>
2023-07-12 19:00:16 -0600

    PhotoEditor code.

---
+++ PhotoEditor/SimplePhotoEditor-main/PhotoEditorSimple/PhotoEditorSimple/Transform.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoEditorSimple
+{
+    public partial class Transform : Form
+    {
+        public bool cancelButtonClicked = false;
+        private CancellationTokenSource cancellationTokenSource;
+
+
+        public Transform()
+        {
+            
+            InitializeComponent();
+            
+        }
+
+        public ProgressBar progressBarAccess
+        {
+            get { return this.progressBar1; }
+            set { this.progressBar1 = value; }
+        }
+
+        public void CancelButton_Click(object sender, EventArgs e)
+        {
+            cancelButtonClicked = true;
+            this.Close();
+        }
+
+        public void progressBar1_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void Transform_Load(object sender, EventArgs e)
+        {
+            cancelButtonClicked = false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# GUI_PhotoEditor
+
+This program is a photo editor. It lets the user browse through files to find photos. After double clicking, the user can modify the photo, using different options such as Invert, Change color and brightness. It also lets the user save the photo with a different name. 
+
+Extra credit: Splitter, icons and Save As.
+
+Teammates contribution:
+Anny:
+Main Form
+About Form
+Helped with transform bugs 
+progress bar function
+fixed errors in edit form
+
+Karen: 
+Edit form
+transform form
+Helped with main form bugs and errors
+Splitter
+Tree view Icons
+Save As button
+
+Percentage:
+Anny:50%
+Karen:50%
